e0040dba11347e6c4d5da4adfa6628a9413da804
mhsn443 <majidpour443@gmail.com>
2025-11-06 05:31:15 +0330

    feat: add header

---
+++ components/navbar-components/logo.tsx
@@ -0,0 +1,14 @@
+export default function Logo() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      xmlSpace="preserve"
+      viewBox="0 0 32 32"
+      width={32}
+      fill="currentColor"
+    >
+      <path d="M28.4 0h-7.2v10.8h7.2v3.6h-7.2v7.2H14v7.2h-3.6v-7.2H0v3.6h7.2v3.6H0V32h32V0zM17.8 28.4v-3.6h3.6v3.6zm10.6 0h-3.6v-3.6h3.6zm0-7.1h-3.6v-3.6h3.6zm0-14.2h-3.6V3.6h3.6z" />
+      <path d="M7.1 17.8h10.7V0H7.1v7.1H3.6V0H0v10.7h10.7V3.6h3.5v10.6H0v3.6h3.6z" />
+    </svg>
+  );
+}
---
+++ data/navbar-menu.ts
@@ -0,0 +1,11 @@
+type NavigationLinks = {
+  hrefe: string;
+  label: string;
+};
+
+export const navigationLinks = [
+  { href: "/", label: "Home" },
+  { href: "/who-is-imam-mahdi", label: "Who is Imam Mahdi?" },
+  { href: "/signs-of-the-reappearance", label: "Signs of the Reappearance" },
+  { href: "/faq", label: "FAQ" },
+];
