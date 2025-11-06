0e74807a82fb6f0d992bc8d1fab44eddff1b3b11
mhsn443 <majidpour443@gmail.com>
2025-11-06 05:31:15 +0330

    feat(homepage): add section about signs of the reappearance

--- app/page.tsx
+++ app/page.tsx
@@ -53,6 +53,56 @@ export default function Home() {
           <ReadMoreButton />
         </Link>
       </section>
+
+      {/* Signs of the reappearance of Imam Mahdi (a.j.) */}
+      <section className="mt-32 flex flex-col gap-y-4 md:mt-48">
+        <h1
+          className="font-bold"
+          style={{ fontSize: "clamp(1.25rem, 0.809rem + 1.471vw, 1.75rem)" }}
+        >
+          Signs of the reappearance of Imam Mahdi (a.j.)
+        </h1>
+        <div className="space-y-4">
+          <p>
+            The signs of the reappearance are divided into definite and
+            non-definite signs. Definite signs are those whose occurrence is
+            certain, and Imam Mahdi (a.j.) will not reappear until they take
+            place. Non-definite signs, on the other hand, are those that are not
+            confirmed to be certain, and the Imam Mahdi (a.j.) may reappear even
+            if they do not occur.
+          </p>
+          <ul className="ml-4 list-inside list-disc">
+            <li>
+              According to a narration reported by Sheikh al-Saduq, the
+              appearance of Sufyani, the uprising of Yamani, the heavenly cry,
+              the killing of the Pure Soul (Nafs al-Zakiyyah), and the
+              swallowing of the army in the desert of Bayda are among the
+              definite signs of the reappearance.
+            </li>
+          </ul>
+          <p>
+            In Judaism and Christianity, certain signs have been mentioned
+            regarding the coming of the savior, some of which are similar to the
+            signs of reappearance mentioned in Islamic traditions.
+          </p>
+          <ul className="ml-4 list-inside list-disc">
+            <li>
+              Jews believe that the spread of corruption and the prevalence of
+              war and chaos are among the signs of the coming of the Messiah
+              (Mashiach).
+            </li>
+            <li>
+              Christians, on the other hand, believe that the Antichrist—or
+              Dajjal—is a person or group who denies or rejects the messiahship
+              of Jesus (pbuh). The Antichrist will rise at the end of times, and
+              when Jesus (pbuh) returns, he will defeat him.
+            </li>
+          </ul>
+        </div>
+        <Link href={"/signs-of-the-reappearance"} className="self-end">
+          <ReadMoreButton />
+        </Link>
+      </section>
     </main>
   );
 }
